a4973a5520534293dd26c2924a3ba04f9a9ab6f2
Thiago Lima <thiago.lima@noredink.com>
2025-01-14 21:19:03 -0300

    feat: add category to exports

--- rest/exports/models.py
+++ rest/exports/models.py
@@ -7,6 +7,7 @@ exports = Model('Export', {
   'id': fields.Integer(required=True, example=6),
   'country': fields.String(required=True, example='Portugal'),
   'country_code': fields.String(required=True, example='PT', description='ISO 3166-1 alpha-2 code'),
+  'category': fields.String(required=True, example='Vinhos de Mesa'),
   'quantities': fields.List(fields.Nested(rest.models.quantity), required=True),
 })
 
--- rest/exports/routes.py
+++ rest/exports/routes.py
@@ -18,6 +18,7 @@ class Export(Resource):
         "id": 6,
         "country": "Portugal",
         "country_code": "PT",
+        "category": "Vinhos de Mesa",
         "quantities": [
           {
             "unit": "kg",
